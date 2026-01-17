faf8dfe403361f7d7d89d05f90827a693da0817f
Jayson-prog <javerdomaraya7@gmail.com>
2026-01-18 05:15:12 +0800

    Remove STATICFILES_STORAGE - let WhiteNoise middleware handle static files

--- core/core/settings.py
+++ core/core/settings.py
@@ -131,7 +131,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.WhiteNoiseStaticFilesStorage'
 
 # Cloudinary Configuration
 import cloudinary
